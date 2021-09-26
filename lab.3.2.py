d0f3d33619b634e14c536f11420d0c2962b2f79c
Nikitosikanikeev <91067792+Nikitosikanikeev@users.noreply.github.com>
2021-09-26 22:21:55 +0300

    1

---
+++ lab.3.2.py
@@ -0,0 +1,4 @@
+file_1 = ["Hello", "advanced", "private", "public"]
+with open("C:/Users/Huawei/Desktop/projects/Project-3/lesson_3.2", "w") as file:
+    file.write("\n".join(file_1))
+file.close()
